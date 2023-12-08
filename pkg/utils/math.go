9f2ccb32a86003d7e899e152924485d55ec192dc
Yorick Smilda <yorick@smilda.tech>
2023-12-08 14:29:15 +0100

    Split up utils pkg, add math GCD and LCM functions

---
+++ pkg/utils/math.go
@@ -0,0 +1,55 @@
+package utils
+
+import "golang.org/x/exp/constraints"
+
+func Min[T constraints.Integer | constraints.Float](a, b T) T {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func Max[T constraints.Integer | constraints.Float](a, b T) T {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func Limit[T constraints.Integer | constraints.Float](value, min, max T) T {
+	return Min(Max(value, min), max)
+}
+
+func Abs[T constraints.Integer | constraints.Float](value T) T {
+	if value < 0 {
+		return -value
+	}
+	return value
+}
+
+func GCD[T constraints.Integer](a, b T) T {
+	if a == 0 {
+		return b
+	}
+	return GCD(b%a, a)
+}
+
+func GCDs[T constraints.Integer](values ...T) T {
+	result := values[0]
+	for _, value := range values[1:] {
+		result = GCD(result, value)
+	}
+	return result
+}
+
+func LCM[T constraints.Integer](a, b T) T {
+	return a * b / GCD(a, b)
+}
+
+func LCMs[T constraints.Integer](values ...T) T {
+	result := values[0]
+	for _, value := range values[1:] {
+		result = LCM(result, value)
+	}
+	return result
+}
